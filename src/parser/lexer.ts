99f8e825e731028dc5ab844e8aefecde09671da1
Matan Lurey <matanlurey@users.noreply.github.com>
2018-09-14 22:55:00 -0700

    Add a partial/untested lexer. (#9)

--- src/parser/lexer.ts
+++ src/parser/lexer.ts
@@ -1,1 +1,40 @@
-export class Lexer {}
+import { Scanner, Token } from '../parser';
+
+/**
+ * Produces a stream of tokens from the @member scanner.
+ *
+ * Implements @type {Iterable<Token>}.
+ */
+export class Lexer implements Iterable<Token> {
+  constructor(private readonly scanner: Scanner) {}
+
+  public *[Symbol.iterator](): Iterator<Token> {
+    while (!this.scanner.isDone) {
+      yield* this.scanCompilationUnit();
+    }
+  }
+
+  /**
+   * Throws a terminal error at the current scanner position.
+   *
+   * @param message
+   */
+  protected fail(message: string): never {
+    throw message;
+  }
+
+  protected *scanCompilationUnit(): Iterable<Token> {
+    yield* this.scanFunction();
+  }
+
+  protected *scanFunction(): Iterable<Token> {
+    yield this.scanIdentifier() || this.fail('Expected identifier');
+  }
+
+  protected scanIdentifier(): Token | undefined {
+    if (this.scanner.scan(/[_a-zA-Z][_a-zA-Z0-9]{0,30}/)) {
+      return new Token(this.scanner.lastMatch![0]);
+    }
+    return undefined;
+  }
+}
---
+++ src/parser/token.ts
@@ -0,0 +1,3 @@
+export class Token {
+  constructor(public readonly value: string) {}
+}
