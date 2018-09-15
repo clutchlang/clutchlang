cff92a0e391b98bbc5d2aaffdbdd0dffdb422f95
Matan Lurey <matan@lurey.org>
2018-09-15 11:55:01 -0700

    Reformat and fix a bit of the lexer.

--- __tests__/lexer.spec.ts
+++ __tests__/lexer.spec.ts
@@ -8,8 +8,8 @@ it('should lex a simple program', () => {
   expect(Array.from(lexer)).toMatchObject([
     new Token(Tokens.Identifier, 'main'),
     new Token(Tokens.Arrow, '=>'),
-    new Token(Tokens.RCurly, '{'),
-    new Token(Tokens.LCurly, '}'),
+    new Token(Tokens.LCurly, '{'),
+    new Token(Tokens.RCurly, '}'),
   ]);
 });
 
@@ -34,6 +34,12 @@ describe('should catch lexing errors', () => {
 
   it('expected }', () => {
     const scanner = new Scanner('main => {$');
+    const lexer = new Lexer(scanner);
+    expect(() => Array.from(lexer)).toThrowError(SyntaxError);
+  });
+
+  it('expected } after new line', () => {
+    const scanner = new Scanner('main => {\n$\n$');
     const lexer = new Lexer(scanner);
     expect(() => Array.from(lexer)).toThrowError(SyntaxError);
   });
