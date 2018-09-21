094c5408a1360dadbadbb84f2bea308aacd9bd7d
Matan Lurey <matan@lurey.org>
2018-09-21 13:07:56 -0700

    Format.

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
   testRegex: '(\\.|/)(test|spec)\\.(jsx?|tsx?)$',
   testPathIgnorePatterns: ['/lib/', '/node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverage: true
+  collectCoverage: true,
 };
