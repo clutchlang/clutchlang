2fc006874a83c22fb45061f999cb449536035dcd
Matan Lurey <matan@lurey.org>
2018-10-17 21:37:44 -0700

    Finished expression v2.

--- src/language/ast/parser/parsers/expression.ts
+++ src/language/ast/parser/parsers/expression.ts
@@ -43,7 +43,7 @@ export class ExpressionParser extends OperatorParser {
       lexer.$DashEquals,
       lexer.$StarEquals,
       lexer.$SlashEquals,
-      lexer.$PercentEquals,
+      lexer.$PercentEquals
     );
   }
 
