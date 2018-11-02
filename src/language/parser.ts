fd981bd4b5fce895077863fac262808d6a23c0ae
Matan Lurey <matan@lurey.org>
2018-11-02 13:17:47 -0700

    Remove unused file.

--- src/language/parser.ts
+++
@@ -1,7 +0,0 @@
-export * from './parser/nodes/expressions';
-export * from './parser/factory';
-export * from './parser/parser';
-export * from './parser/nodes/nodes';
-export * from './parser/nodes/precedence';
-export * from './parser/nodes/statements';
-export * from './parser/visitors';
